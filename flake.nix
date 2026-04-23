235f4b98b3852f2277e3a17d2c591906741e2cef
lou lecrivain <lou.lecrivain@wdz.de>
2026-04-23 15:57:00 +0200

    add flake tools

--- flake.nix
+++ flake.nix
@@ -22,19 +22,53 @@
         inherit system;
         overlays = [ self.overlay ];
       };
-    in
-      {
-        packages = {
-          routefilterd = pkgs.routefilterd;
-          default = pkgs.routefilterd;
+      minimal-dev-pkgs = with pkgs; [
+        clippy
+        cargo
+        rustc
+        libllvm
+        grcov
+        lcov
+      ];
+      coverageScript = pkgs: pkgs.writeShellApplication {
+        name = "gen-coverage";
+        runtimeInputs = minimal-dev-pkgs;
+        runtimeEnv = {
+          LLVM_PROFILE_FILE = "grcov-%p-%m.profraw";
+          RUSTFLAGS = "-Cinstrument-coverage -Copt-level=0 -Coverflow-checks=off";
         };
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-          ];
-        };
-      }
+        # --llvm-path is needed since libllvm
+        #   will be in nix store,
+        #   which grcov doesn't expect.
+        # --html-resources cdn is needed
+        #   bc we don't have a local HTTP server
+        text = ''
+             cargo test
+             mkdir -p target/coverage
+             grcov "$(find . -name 'grcov-*.profraw' -print)" \
+                --llvm-path ${pkgs.libllvm}/bin \
+                --source-dir src \
+                --binary-path target/debug/ \
+                --output-types lcov,html \
+                --html-resources cdn \
+                --branch --ignore-not-existing \
+                --ignore "/*" \
+                -o target/coverage/
+             lcov -l target/coverage/lcov
+             rm ./*.profraw
+             '';
+      };
+    in {
+      packages = {
+        routefilterd = pkgs.routefilterd;
+        default = pkgs.routefilterd;
+        gen_coverage = coverageScript pkgs;
+      };
+      devShell = pkgs.mkShell {
+        LLVM_COV = "${pkgs.libllvm}/bin/llvm-cov"; # needed for cargo-llvm-cov
+        LLVM_PROFDATA = "${pkgs.libllvm}/bin/llvm-profdata"; # same as above
+        nativeBuildInputs = minimal-dev-pkgs ++ [ pkgs.cargo-llvm-cov (coverageScript pkgs) ];
+      };
+    }
   ));
 }
