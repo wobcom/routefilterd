702706283ad2b82c71f6d3abd19e07134ba75731
lou lecrivain <lou.lecrivain@wdz.de>
2026-04-30 16:03:52 +0200

    test no result

--- src/tests/store.rs
+++ src/tests/store.rs
@@ -3,6 +3,8 @@ use crate::tests::{fixtures, util};
 const WMF_AS_SET: &str = "AS-WIKIMEDIA-ARIN";
 const WMF_HQ_AS: &str = "AS11820";
 const WMF_WP_AS: &str = "AS14907";
+const RESERVED_PRIV_AS: &str = "AS65534";
+const UNKNOWN_AS_SET: &str = "AS-WOBCOM3000";
 
 #[tokio::test]
 async fn test_asn_query() {
@@ -29,6 +31,19 @@ async fn test_asn_query() {
 
     assert_eq!(query_result.prefixes, wmd_prefixes);
 }
+
+#[tokio::test]
+#[should_panic]
+async fn test_asn_query_no_result() {
+    let store = fixtures::get_new_store();
+
+    util::import_file_in_store(&store, "data/arin_small.db", "arin", 200).await;
+
+    let _ = store
+        .query_asn(vec![String::from("arin")], String::from(RESERVED_PRIV_AS))
+        .expect("test query did not sent back any routes");
+}
+
 #[tokio::test]
 async fn test_as_set_shallow_query() {
     let wmd_as_set = String::from(WMF_AS_SET);
@@ -48,4 +63,18 @@ async fn test_as_set_shallow_query() {
         util::sorted_vec(vec![WMF_WP_AS, WMF_HQ_AS])
     );
     assert_eq!(query_result.as_sets.len(), 0); // not recursive
+}
+
+#[tokio::test]
+#[should_panic]
+async fn test_as_set_shallow_query_no_result() {
+    let unknown_as_set = String::from(UNKNOWN_AS_SET);
+
+    let store = fixtures::get_new_store();
+
+    util::import_file_in_store(&store, "data/arin_small.db", "arin", 200).await;
+
+    let _ = store
+        .query_as_set(vec![], unknown_as_set.clone(), &[])
+        .expect("query did not sent back any as-set");
 }
