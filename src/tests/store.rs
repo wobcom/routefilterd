316ede8e1771cf0c9c81858a6a97188897469fc3
lou lecrivain <lou.lecrivain@wdz.de>
2026-05-04 12:10:42 +0200

    add as set multisource query test

--- src/tests/store.rs
+++ src/tests/store.rs
@@ -1,6 +1,10 @@
 use crate::tests::{fixtures, util};
 
 const WMF_AS_SET: &str = "AS-WIKIMEDIA-ARIN";
+const TELEMEDIA_AS_SET: &str = "AS-TELEMEDIA";
+
+const TELEMEDIA_ASNS: &[&str] = &["AS16625", "AS132091", "AS17810", "AS132075", "AS45284"];
+const TELEMEDIA_AS_SETS: &[&str] = &["AS-DATACON-IN", "AS55353:AS-RPNET-RACKBANK-GIGABITPORT"];
 const WMF_HQ_AS: &str = "AS11820";
 const WMF_WP_AS: &str = "AS14907";
 const RESERVED_PRIV_AS: &str = "AS65534";
@@ -77,4 +81,29 @@ async fn test_as_set_shallow_query_no_result() {
     let _ = store
         .query_as_set(vec![], unknown_as_set.clone(), &[])
         .expect("query did not sent back any as-set");
+}
+
+#[tokio::test]
+async fn test_as_set_overlapping_datasources() {
+    let telemedia_as_set = String::from(TELEMEDIA_AS_SET);
+
+    let store = fixtures::get_new_store();
+
+    // redundant definition, radb also has another telemedia
+    util::import_file_in_store(&store, "data/radb_small.db", "radb", 100).await;
+    util::import_file_in_store(&store, "data/apnic_small.db", "apnic", 200).await;
+
+    let query_result = store
+        .query_as_set(vec![], telemedia_as_set.clone(), &[])
+        .expect("query did not sent back any result");
+
+    assert_eq!(
+        util::sorted_vec(query_result.asns),
+        util::sorted_vec(Vec::from(TELEMEDIA_ASNS))
+    );
+
+    assert_eq!(
+        util::sorted_vec(query_result.as_sets),
+        util::sorted_vec(Vec::from(TELEMEDIA_AS_SETS))
+    );
 }
