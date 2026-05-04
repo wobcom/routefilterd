3c2331617aedf7ba8874e26c45193e79aa0b19ef
lou lecrivain <lou.lecrivain@wdz.de>
2026-05-04 12:21:18 +0200

    cleanup test code

--- src/tests/store.rs
+++ src/tests/store.rs
@@ -1,6 +1,15 @@
 use crate::tests::{fixtures, util};
 
 const WMF_AS_SET: &str = "AS-WIKIMEDIA-ARIN";
+const WMF_PREFIXES: &[&str] = &[
+    "198.35.26.0/24",
+    "198.35.27.0/24",
+    "208.80.152.0/23",
+    "208.80.154.0/23",
+    "2620:0:860::/48",
+    "2620:0:861::/48",
+    "2620:0:863::/48",
+];
 const TELEMEDIA_AS_SET: &str = "AS-TELEMEDIA";
 
 const TELEMEDIA_ASNS: &[&str] = &["AS16625", "AS132091", "AS17810", "AS132075", "AS45284"];
@@ -16,24 +25,15 @@ async fn test_asn_query() {
 
     util::import_file_in_store(&store, "data/arin_small.db", "arin", 200).await;
 
-    let mut query_result = store
+    let query_result = store
         .query_asn(vec![String::from("arin")], String::from(WMF_WP_AS))
         .expect("test query did not sent back any routes")
         .clone();
 
-    let mut wmd_prefixes = vec![
-        String::from("198.35.26.0/24"),
-        String::from("198.35.27.0/24"),
-        String::from("208.80.152.0/23"),
-        String::from("208.80.154.0/23"),
-        String::from("2620:0:860::/48"),
-        String::from("2620:0:861::/48"),
-        String::from("2620:0:863::/48"),
-    ];
-    wmd_prefixes.sort();
-    query_result.prefixes.sort();
-
-    assert_eq!(query_result.prefixes, wmd_prefixes);
+    assert_eq!(
+        util::sorted_vec(query_result.prefixes),
+        util::sorted_vec(Vec::from(WMF_PREFIXES))
+    );
 }
 
 #[tokio::test]
@@ -43,7 +43,7 @@ async fn test_asn_query_no_result() {
 
     util::import_file_in_store(&store, "data/arin_small.db", "arin", 200).await;
 
-    let _ = store
+    store
         .query_asn(vec![String::from("arin")], String::from(RESERVED_PRIV_AS))
         .expect("test query did not sent back any routes");
 }
@@ -56,14 +56,12 @@ async fn test_as_set_shallow_query() {
 
     util::import_file_in_store(&store, "data/arin_small.db", "arin", 200).await;
 
-    let mut query_result = store
+    let query_result = store
         .query_as_set(vec![], wmd_as_set.clone(), &[])
         .expect("query did not sent back any as-set");
 
-    query_result.asns.sort();
-
     assert_eq!(
-        query_result.asns,
+        util::sorted_vec(query_result.asns),
         util::sorted_vec(vec![WMF_WP_AS, WMF_HQ_AS])
     );
     assert_eq!(query_result.as_sets.len(), 0); // not recursive
@@ -78,7 +76,7 @@ async fn test_as_set_shallow_query_no_result() {
 
     util::import_file_in_store(&store, "data/arin_small.db", "arin", 200).await;
 
-    let _ = store
+    store
         .query_as_set(vec![], unknown_as_set.clone(), &[])
         .expect("query did not sent back any as-set");
 }
