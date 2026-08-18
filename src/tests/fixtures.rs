23b4114d4601e8ad5b4939e17469a28ce997fbbc
lou lecrivain <lou.lecrivain@wdz.de>
2026-08-18 18:20:37 +0200

    test code for FTP fetch support

--- src/tests/fixtures.rs
+++ src/tests/fixtures.rs
@@ -1,6 +1,30 @@
 use crate::store::DataStore;
+use libunftp::options::{ActivePassiveMode, Shutdown};
+use libunftp::{Server, ServerBuilder, options};
+use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::Duration;
+use tokio::sync::mpsc::Receiver;
+use unftp_core::auth::UserDetail;
+use unftp_sbe_fs::Filesystem;
 
 pub fn get_new_store() -> Arc<DataStore> {
     Arc::new(DataStore::new())
+}
+
+pub fn get_new_stoppable_ftp_server_with_fs_path(
+    path: PathBuf,
+    mut channel_recv: Receiver<()>,
+) -> Server<Filesystem, impl UserDetail> {
+    ServerBuilder::new(Box::new(move || Filesystem::new(path.clone()).unwrap()))
+        .greeting("Test FTP Server")
+        .passive_ports(50000..=65535)
+        .passive_host(options::PassiveHost::FromConnection)
+        .active_passive_mode(ActivePassiveMode::ActiveAndPassive)
+        .shutdown_indicator(async move {
+            channel_recv.recv().await.unwrap(); // block on shutdown message recv
+            Shutdown::new().grace_period(Duration::from_millis(10))
+        })
+        .build()
+        .unwrap()
 }
